bce5c9aac2baac33f92cbbe173b8edf4d31aa3ff
retocali <retocali@mit.edu>
2018-04-19 12:21:27 -0400

    Did the main page and added some starter code for some things

---
+++ js/main.js
@@ -0,0 +1,26 @@
+Util.events(document, {
+	// Final initalization entry point: the Javascript code inside this block
+	// runs at the end of start-up when the DOM is ready
+	"DOMContentLoaded": function() {
+		
+	},
+
+	// Keyboard events arrive here
+	"keydown": function(evt) {
+
+    },
+
+    // Click events arrive here
+	"click": function(evt) {
+
+    },
+	"mousedown": function(evt) {
+
+    },
+	"mousemove": function(evt) {
+
+    },
+	"mouseup": function(evt) {
+
+    }
+});
