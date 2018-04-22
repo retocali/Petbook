79d9059494349c4d424390622d56fe9d3c88c17f
araadhikari <adhikara@mit.edu>
2018-04-22 13:24:29 -0400

    uhh

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-import Util from 'js/util.js';
-
 Util.events(document, {
 	// Final initalization entry point: the Javascript code inside this block
 	// runs at the end of start-up when the DOM is ready
 	"DOMContentLoaded": function() {
-		console.log('hello??');
+		var rawfile = new XMLHttpRequest();
+		rawfile.open("GET","profiles/example.json",true);
+
+		rawfile.onreadystatechange = function () {               
+           if (rawfile.readyState == 4) {                   
+             var lines = rawfile.responseText;    //*here we get all lines from text file*
+             console.log(lines);
+             intoArray(lines);     *//here we call function with parameter "lines*"                   
+           }               
+       }
 	},
 
 	// Keyboard events arrive here
