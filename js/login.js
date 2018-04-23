26c94360b7164ee3c98913b81670e1bbdd9ed421
retocali <retocali@mit.edu>
2018-04-22 22:47:43 -0400

    Made login the coolest thing in the world

---
+++ js/login.js
@@ -0,0 +1,44 @@
+
+Util.events(document, {
+	// Final initalization entry point: the Javascript code inside this block
+	// runs at the end of start-up when the DOM is ready
+	"DOMContentLoaded": function() {
+		if (State.isSignedIn()) {
+			document.getElementById("signupmessage").style.display = "inline"
+		}
+		document.getElementById("reset").addEventListener(
+			"click", () => {
+				document.getElementById("form").reset();		
+			}
+		);		
+		document.getElementById("form").onsubmit = ()=>{submit()};
+	},
+
+	// Keyboard events arrive here
+	"keydown": function(evt) {
+
+    },
+
+    // Click events arrive here
+	"click": function(evt) {
+	
+    },
+	"mousedown": function(evt) {
+
+    },
+	"mousemove": function(evt) {
+
+    },
+	"mouseup": function(evt) {
+
+    }
+});
+
+function submit() {
+	State.signIn();
+	console.log(State.isSignedIn());
+	if (State.isSignedIn()) {
+		document.getElementById("signupmessage").style.display = "inline";
+	}
+}
+
