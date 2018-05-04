5167a4a13977c9e4cf5d50a5d41dfdb3b96a1730
retocali <retocali@mit.edu>
2018-05-04 18:49:44 -0400

    Fixed more my-profile things plus started on search bar

--- js/my_profile.js
+++ js/my_profile.js
@@ -41,11 +41,16 @@ Util.events(document, {
     }
 });
 function showMyProfile() {
-    document.getElementById("username").innerHTML = "USERNAME: " + State.getUsername();
-    document.getElementById("fullName").innerHTML = "FULL NAME: "+ State.getFullName();
-    document.getElementById("location").innerHTML = "LOCATION: " + State.getLocation();
-    document.getElementById("status").innerHTML = "STATUS: " + State.getStatus();
-    document.getElementById("bio").innerHTML = "BIO: " + State.getBio();
+    // document.getElementById("username").innerHTML = "USERNAME: " + State.getUsername();
+    // document.getElementById("fullName").innerHTML = "FULL NAME: "+ State.getFullName();
+    // document.getElementById("location").innerHTML = "LOCATION: " + State.getLocation();
+    // document.getElementById("status").innerHTML = "STATUS: " + State.getStatus();
+    // document.getElementById("bio").innerHTML = "BIO: " + State.getBio();
+    document.getElementById("username").value = State.getUsername();
+    document.getElementById("fullName").value = State.getFullName();
+    document.getElementById("location").value = State.getLocation();
+    document.getElementById("status").value = State.getStatus();
+    document.getElementById("bio").value = State.getBio();
 
 }
 function showMyPets() {
@@ -54,24 +59,41 @@ function showMyPets() {
         return;
     }
     
-    let addedPets = document.getElementsByClassName("addedPets");
-    console.log(addedPets);
-    for (let i = 0; i < addedPets.length; i++) {
-        let element = addedPets[i];
-        element.remove()
-    }
+    // let addedPets = document.getElementsByClassName("addedPets");
+    // console.log(addedPets);
+    // for (let i = 0; i < addedPets.length; i++) {
+    //     let element = addedPets[i];
+    //     element.remove()
+    // }
+    
     let card = document.getElementById("mypets");
     let node = document.getElementById("addPet");
+    let newAdd = node.cloneNode(true);
+    while (card.firstChild) {
+        card.removeChild(card.firstChild)
+    }
+    
     for (let i = 0; i < allPets.length; i++) {
         const element = allPets[i];
-        let newNode = node.cloneNode(true);
+        let newNode = newAdd.cloneNode(true);
         let image = newNode.children[1];
+
         image.href.baseVal = "profiles/sample/sample-" + element.catagory + ".svg";
+       
+        image.setAttribute("height", "10vh");
+        image.setAttribute("width", "10vh");
+        image.setAttribute("x", "3vh");
+        image.setAttribute("y", "3vh");
+       
+        newNode.children[3].innerHTML = element.petName;
+       
         newNode.id = "";
+       
         newNode.classList.add("addedPets");
-        newNode.appendChild(image);
-        card.insertBefore(newNode, node);
+        card.appendChild(newNode);
     }
+    card.appendChild(newAdd);
+
 }
 
 function addPet() {
