c13d396ecaa84f4629b076e992cf8137d8d33dfe
retocali <retocali@mit.edu>
2018-05-05 10:50:52 -0400

    First draft for search

--- js/results.js
+++ js/results.js
@@ -5,7 +5,41 @@ Util.events(document, {
 		data = new Data();
         let query = getQueryString("search");
         console.log(query);
-        document.getElementById("searchResults").innerHTML = "Results for <text style='color: #8BCBC8'>"+ query + "</text>";
+        document.getElementById("searchResults").innerHTML = 
+                    "Results for <text style='color: #8BCBC8'>"+ query + "</text><div id='results'></div>";
+        showResults(query);
 	},
     
 }); 
+
+function showResults(query) {
+    let results = document.getElementById("results");
+    switch (query) {
+        case "dog":
+            results.innerHTML = generateResults(data.dogs);
+            return;
+        case "cat":
+            results.innerHTML = generateResults(data.cats);
+            return;
+        case "bird":
+            results.innerHTML = generateResults(data.birds);
+            return;
+        case "fish":
+            results.innerHTML = generateResults(data.fish);
+            return; 
+        case "reptile":
+            results.innerHTML = generateResults(data.reptiles);
+            return;
+    }
+    
+}
+
+function generateResults(data) {
+    let resultsHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        let element = data[i];
+        resultsHTML += "<a href=social_pets.html?catagory=" + element.catagory +
+                        "&petCount=" + i + " id='resultLink'>" + element.petName + "</a><br>";
+    }
+    return resultsHTML;
+}
